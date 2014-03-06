3b9ff41cbb59cecfd313ee430345f72faba931c4
Robert Farrelly <hello@robertfarrelly.com>
2014-03-06 20:13:55 +0000

    full days work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,3 @@
-console.log('\'Allo \'Allo!');
-
 // window.mySwipe = new Swipe(document.getElementById('slider'), {
 //   startSlide: 2,
 //   speed: 400,
@@ -11,8 +9,9 @@ console.log('\'Allo \'Allo!');
 //   transitionEnd: function(index, elem) {}
 // });
 
+if($('body').hasClass('home')){
 
-var slider =
+  var slider =
   Swipe(document.getElementById('slider'), {
     auto: 5000,
     continuous: true,
@@ -28,3 +27,18 @@ var slider =
   });
 
 var bullets = document.getElementById('position').getElementsByTagName('li');
+
+
+}
+
+
+$(document).ready(function(){
+  $("#accordian h4").click(function(){
+    $("#accordian ul ul").slideUp();
+    //slide down the link list below the h3 clicked - only if its closed
+    if(!$(this).next().is(":visible"))
+    {
+      $(this).next().slideDown();
+    }
+  });
+});
